1ef9b8c7326ee8b0a5aaaea5285ce933201d470b
Kristian S. Stene <kristian.stene@spv.no>
2025-02-28 07:44:58 +0100

    working custom instrumentation trace and metric

--- src/frontend/otelConfig.js
+++ src/frontend/otelConfig.js
@@ -1,84 +1,82 @@
+const opentelemetry = require('@opentelemetry/api');
 const { NodeSDK } = require('@opentelemetry/sdk-node');
 const { getNodeAutoInstrumentations } = require('@opentelemetry/auto-instrumentations-node');
-const { OTLPTraceExporter } = require('@opentelemetry/exporter-trace-otlp-http');
-const { OTLPMetricExporter } = require('@opentelemetry/exporter-metrics-otlp-http');
-const { diag, DiagConsoleLogger, DiagLogLevel } = require('@opentelemetry/api');
+const { ConsoleSpanExporter } = require('@opentelemetry/sdk-trace-node');
 const { MeterProvider, PeriodicExportingMetricReader, ConsoleMetricExporter } = require('@opentelemetry/sdk-metrics');
+const { OTLPTraceExporter } = require('@opentelemetry/exporter-trace-otlp-proto');
+const { OTLPMetricExporter } = require('@opentelemetry/exporter-metrics-otlp-proto');
+const { diag, DiagConsoleLogger, DiagLogLevel } = require('@opentelemetry/api');
 const { Resource } = require('@opentelemetry/resources');
-const { ATTR_SERVICE_NAME } = require('@opentelemetry/semantic-conventions');
+const { ATTR_SERVICE_NAME, ATTR_SERVICE_VERSION } = require('@opentelemetry/semantic-conventions');
 
 // Set up diagnostic logging
 diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.DEBUG);
 
-const resource = Resource.default().merge(
-  new Resource({
-    [ATTR_SERVICE_NAME]: 'otel-workshop-frontend'
-  }),
-);
-
 // Read the OTel collector URL from environment variables
-const otelEndpoint = process.env.OTEL_COLLECTOR_URL;
+const otelEndpoint = process.env.OTEL_EXPORTER_OTLP_ENDPOINT;
 if (!otelEndpoint) {
-  console.error('OTEL_COLLECTOR_URL environment variable is not set.');
+  console.error('OTEL_EXPORTER_OTLP_ENDPOINT environment variable is not set.');
   process.exit(1);
 }
 
-// Configure the OTLP exporter
+// Define a custom resource attributes
+const resource = Resource.default().merge(
+  new Resource({
+    [ATTR_SERVICE_NAME]: 'otel-workshop-frontend',
+    [ATTR_SERVICE_VERSION]: '0.0.1',
+  }),
+);
+
+// A custom trace exporter that sends traces to the OTel collector
 const traceExporter = new OTLPTraceExporter({
+  // optional - default url is http://localhost:4318/v1/traces
   url: `${otelEndpoint}/v1/traces`,
-  headers: {
-    Authorization: process.env.OTEL_AUTH_HEADER || '', // Optional: If authentication is required
-  },
-});
+  // optional - collection of custom headers to be sent with each request, empty by default
+  headers: {},
+})
 
-const metricExporter = new OTLPMetricExporter({
-  url: `${otelEndpoint}/v1/metrics`,
-  headers: {
-    Authorization: process.env.OTEL_AUTH_HEADER || '', // Optional: If authentication is required
-  },
-});
-
-// Initialize the OpenTelemetry SDK
-const sdk = new NodeSDK({
-  resource: resource,
-  traceExporter,
-  metricExporter,
-  instrumentations: [getNodeAutoInstrumentations()],
-  serviceName: 'my-nodejs-service',
-});
-
-// Start the SDK
-(async () => {
-  try {
-      await sdk.start();
-      console.log('OpenTelemetry SDK started.');
-  } catch (error) {
-    console.error('Error starting OpenTelemetry SDK:', error);
-  }
-})();
-
-// METRICS
-// Set up the metric reader
+// A custom metric reader that exports metrics every 10 seconds
 const metricReader = new PeriodicExportingMetricReader({
-  exporter: new ConsoleMetricExporter(),
+  exporter: new OTLPMetricExporter({
+    url: `${otelEndpoint}/v1/metrics`, // url is optional and can be omitted - default is http://localhost:4318/v1/metrics
+    headers: {}, // an optional object containing custom headers to be sent with each request
+    concurrencyLimit: 1, // an optional limit on pending requests
+  }),
+  // Default is 60000ms (60 seconds). Set to 30 seconds for demonstrative purposes only.
+  exportIntervalMillis: 30000,
+})
 
-  // Default is 60000ms (60 seconds). Set to 10 seconds for demonstrative purposes only.
-  exportIntervalMillis: 10000,
-});
-
-const myServiceMeterProvider = new MeterProvider({
+// Create a MeterProvider with the custom metric reader
+const meterProvider = new MeterProvider({
   resource: resource,
   readers: [metricReader],
 });
 
 // Set this MeterProvider to be global to the app being instrumented.
-opentelemetry.metrics.setGlobalMeterProvider(myServiceMeterProvider);
+opentelemetry.metrics.setGlobalMeterProvider(meterProvider);
 
+// // Initialize the OpenTelemetry SDK
+// const sdk = new NodeSDK({
+//   resource: resource,
+//   traceExporter: traceExporter,
+//   metricReader: metricReader,
+//   instrumentations: [getNodeAutoInstrumentations()],
+// });
 
-// Gracefully shut down the SDK on process exit
-process.on('SIGTERM', () => {
-  sdk.shutdown()
-    .then(() => console.log('OpenTelemetry SDK shut down'))
-    .catch((error) => console.error('Error shutting down OpenTelemetry SDK:', error))
-    .finally(() => process.exit(0));
-});
+// // Start the SDK
+// (async () => {
+//   try {
+//       await sdk.start();
+//       console.log('OpenTelemetry SDK started.');
+//   } catch (error) {
+//     console.error('Error starting OpenTelemetry SDK:', error);
+//   }
+// })();
+
+// // Gracefully shut down the SDK on process exit
+// process.on('SIGTERM', () => {
+//   sdk.shutdown()
+//     .then(() => console.log('OpenTelemetry SDK shut down'))
+//     .catch((error) => console.error('Error shutting down OpenTelemetry SDK:', error))
+//     .finally(() => process.exit(0));
+// });
--- src/frontend/server.js
+++ src/frontend/server.js
@@ -1,61 +1,55 @@
 const express = require('express');
 const path = require('path');
-const { trace } = require('@opentelemetry/api');
 const app = express();
 const port = 3000;
+const { trace, metrics } = require('@opentelemetry/api');
 
-// Import the OpenTelemetry configuration
-require('./otelConfig');
+// Aquire a Tracer and a Meter for the frontend service
+const tracer = trace.getTracer('otel-workshop-frontend','0.0.1');
+const meter = metrics.getMeter('otel-workshop-frontend','0.0.1');
+
+// Define some counter metrics
+const counter = meter.createCounter(
+  'otel.workshop.button.click.counter',
+  {
+    description: 'A counter of button clicks',
+  },
+);
 
 // Serve static files from the "public" directory
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Define a simple REST API endpoint
 app.get('/api/button1', (req, res) => {
-  const tracer = trace.getTracer('button-tracer');
-  const span = tracer.startSpan('button1-clicked');
+  counter.add(1, { button: 'button1' });
   try {
     // Simulate some work
     res.json({ message: 'Button 1 clicked!' });
   } catch (error) {
-    span.setStatus({ code: trace.SpanStatusCode.ERROR, message: error.message });
-    span.end();
     console.error('Error handling button1 click:', error);
     res.status(500).json({ error: 'Internal Server Error' });
-  } finally {
-    span.end();
   }
 });
 
 app.get('/api/button2', (req, res) => {
-  const tracer = trace.getTracer('button-tracer');
-  const span = tracer.startSpan('button2-clicked');
+  counter.add(1, { button: 'button2' });
   try {
     // Simulate some work
     res.json({ message: 'Button 2 clicked!' });
   } catch (error) {
-    span.setStatus({ code: trace.SpanStatusCode.ERROR, message: error.message });
-    span.end();
     console.error('Error handling button2 click:', error);
     res.status(500).json({ error: 'Internal Server Error' });
-  } finally {
-    span.end();
   }
 });
 
 app.get('/api/button3', (req, res) => {
-  const tracer = trace.getTracer('button-tracer');
-  const span = tracer.startSpan('button3-clicked');
+  counter.add(1, { button: 'button3' });
   try {
     // Simulate some work
     res.json({ message: 'Button 3 clicked!' });
   } catch (error) {
-    span.setStatus({ code: trace.SpanStatusCode.ERROR, message: error.message });
-    span.end();
     console.error('Error handling button3 click:', error);
     res.status(500).json({ error: 'Internal Server Error' });
-  } finally {
-    span.end();
   }
 });
 
