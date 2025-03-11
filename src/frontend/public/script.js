31c2d432fad779756681f5df8a089df6c2fa4fdf
Anders Runningen <anders.runningen@spv.no>
2025-03-11 09:48:38 +0100

    Fjerne gammel frontend mappe og raname otel frontend

--- src/frontend/public/script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('button1').addEventListener('click', () => {
-  fetch('https://otel-api.svai.dev/fact')
-    .then(response => response.json())
-    .then(data => alert(data.message))
-    .catch(error => {
-      console.error('Error clicking button 1:', error);
-      alert('An error occurred while clicking button 1');
-    });
-});
